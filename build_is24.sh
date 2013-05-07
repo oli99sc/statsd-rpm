4b74503ded5d7cf5f8921998768b2c1e7f162813
Oliver Schmitz-Hennemann <oli99sc@gmail.com>
2013-05-07 23:04:07 +0200

    continue build script for is24 version

--- build_is24.sh
+++ build_is24.sh
@@ -13,14 +13,12 @@ echo "statsd sha $STATSD_SHA"
 echo "statds-rpm sha $STATSD_RPM_SHA"
 echo "is24-statds sha $IS24_STATSD_SHA"
 
-mkdir -p statsd-$DATE
-rsync -av statsd/ statsd-$DATE --exclude=.git
-mkdir -p statsd-$DATE/redhat
-cat is24-statsd/redhat/is24-statsd | sed -e "s#is24-##g" -e "s#is24 ##g" > statsd-$DATE/redhat/statsd
-tar -cvzf statsd-$DATE.tar.gz statsd-$DATE
-rm -rf statsd-$DATE
-cat statsd.spec.in | sed -e "s/DATE/$DATE/" -e "s/RELEASE_ID/${STATSD_RPM_SHA:0:10}/" -e "s/IS24_STATSD_SHA/$IS24_STATSD_SHA/" -e "s/STATSD_SHA/$STATSD_SHA/" -e "s/STATSD_RPM_SHA/$STATSD_RPM_SHA/" > statsd.spec
+mkdir -p is24-statsd-$DATE
+rsync -av is24-statsd/ is24-statsd-$DATE --exclude=.git
+tar -cvzf is24-statsd-$DATE.tar.gz statsd-$DATE
+rm -rf is24-statsd-$DATE
+cat is24-statsd.spec.in | sed -e "s/DATE/$DATE/" -e "s/RELEASE_ID/${STATSD_RPM_SHA:0:10}/" -e "s/IS24_STATSD_SHA/$IS24_STATSD_SHA/" -e "s/STATSD_SHA/$STATSD_SHA/" -e "s/STATSD_RPM_SHA/$STATSD_RPM_SHA/" > is24-statsd.spec
 mkdir -p ~/rpmbuild/SOURCES
-mv statsd-$DATE.tar.gz ~/rpmbuild/SOURCES
-rpmbuild -ba statsd.spec
-rm statsd.spec 
+mv is24-statsd-$DATE.tar.gz ~/rpmbuild/SOURCES
+rpmbuild -ba is24-statsd.spec
+rm is24-statsd.spec 
