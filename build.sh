10763154df3bc65c4381eaae7ff23346605c9a02
Oliver Schmitz-Hennemann <oli99sc@gmail.com>
2013-05-07 22:29:38 +0200

    make output more readable

--- build.sh
+++ build.sh
@@ -2,16 +2,16 @@ git submodule init
 git submodule update
 
 DATE=`date +%Y%m%d`
-STATSD_RPM_SHA=$(git show-ref HEAD)
+STATSD_RPM_SHA=$(git show-ref HEAD --hash)
 cd statsd
-STATSD_SHA=$(git show-ref HEAD)
+STATSD_SHA=$(git show-ref HEAD --hash)
 cd ../is24-statsd
-IS24_STATSD_SHA=$(git show-ref HEAD)
+IS24_STATSD_SHA=$(git show-ref HEAD --hash)
 cd ..
 
-echo $STATSD_SHA
-echo $STATSD_RPM_SHA
-echo $IS24_STATSS_SHA
+echo "statsd sha $STATSD_SHA"
+echo "statds-rpm sha $STATSD_RPM_SHA"
+echo "is24-statds sha $IS24_STATSD_SHA"
 
 mkdir -p statsd-$DATE
 rsync -av statsd/ statsd-$DATE --exclude=.git
