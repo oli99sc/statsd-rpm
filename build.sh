edfc0080650d2fcdc4d9f967d46d06bc6c4396d0
Oliver Schmitz-Hennemann <oli99sc@gmail.com>
2013-05-07 22:25:14 +0200

    add sha summs tp description and use as release id

--- build.sh
+++ build.sh
@@ -2,6 +2,16 @@ git submodule init
 git submodule update
 
 DATE=`date +%Y%m%d`
+STATSD_RPM_SHA=$(git show-ref HEAD)
+cd statsd
+STATSD_SHA=$(git show-ref HEAD)
+cd ../is24-statsd
+IS24_STATSD_SHA=$(git show-ref HEAD)
+cd ..
+
+echo $STATSD_SHA
+echo $STATSD_RPM_SHA
+echo $IS24_STATSS_SHA
 
 mkdir -p statsd-$DATE
 rsync -av statsd/ statsd-$DATE --exclude=.git
@@ -9,7 +19,7 @@ mkdir -p statsd-$DATE/redhat
 cat is24-statsd/redhat/is24-statsd | sed -e "s#is24-##g" -e "s#is24 ##g" > statsd-$DATE/redhat/statsd
 tar -cvzf statsd-$DATE.tar.gz statsd-$DATE
 rm -rf statsd-$DATE
-cat statsd.spec.in | sed -e "s/DATE/$DATE/" > statsd.spec
+cat statsd.spec.in | sed -e "s/DATE/$DATE/" -e "s/RELEASE_ID/${STATSD_RPM_SHA:0:10}/" -e "s/STATSD_SHA/$STATSD_SHA/" -e "s/IS24_STATSD_SHA/$IS24_STATSD_SHA/" -e "s/STATSD_RPM_SHA/$STATSD_RPM_SHA/" > statsd.spec
 mkdir -p ~/rpmbuild/SOURCES
 mv statsd-$DATE.tar.gz ~/rpmbuild/SOURCES
 rpmbuild -ba statsd.spec
