13394dc2ce17d82caed6a7d8e8c22918772ca4d7
Oliver Schmitz-Hennemann <oli99sc@gmail.com>
2013-05-07 20:21:53 +0200

    prepare for first build trial

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ DATE=`date +%Y%m%d`
 mkdir -p statsd-$DATE
 rsync -av statsd/ statsd-$DATE --exclude=.git
 mkdir -p statsd-$DATE/redhat
-cat is24-statsd/redhat/statsd | sed -e "s#is24-##g" -e "s#is24 ##g" > statsd-$DATE/redhat/statsd
+cat is24-statsd/redhat/is24-statsd | sed -e "s#is24-##g" -e "s#is24 ##g" > statsd-$DATE/redhat/statsd
 tar -cvzf statsd-$DATE.tar.gz statsd-$DATE
 rm -rf statsd-$DATE
 cat statsd.spec.in | sed -e "s/DATE/$DATE/" > statsd.spec
